c2f7560379f03ae942f5206f6fdb38960331f648
kumag36 <>
2026-03-31 00:33:46 +0900

    top page

--- app/page.js
+++ app/page.js
@@ -1,29 +1,13 @@
-﻿export default function Page() {
+﻿import Link from 'next/link'
+
+export default function Home() {
   return (
-    <div style={{
-      background: '#e6f7ff',
-      minHeight: '100vh',
-      padding: 20,
-      textAlign: 'center'
-    }}>
-      
-      <h1 style={{ fontSize: 32 }}>🌱 しょくぶつずかん</h1>
+    <div style={{ padding: 20 }}>
+      <h1>🌱 アロイド図鑑</h1>
 
-      <p>みたい しょくぶつを えらんでね！</p>
-
-      <a href="/plant/test" style={{
-        display: 'block',
-        marginTop: 30,
-        background: '#4CAF50',
-        color: 'white',
-        padding: '20px',
-        borderRadius: '15px',
-        fontSize: '20px',
-        textDecoration: 'none'
-      }}>
-        🌿 テストページ
-      </a>
-
+      <Link href="/plant">
+        👉 図鑑を見る
+      </Link>
     </div>
   )
 }
