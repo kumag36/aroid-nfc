b9864fee7e39f05b1fe58e4f6f2a65e73c783248
kumag36 <kumajuko@gmail.com>
2026-03-31 09:02:15 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,3 @@
-﻿export const metadata = {
-  title: 'Aroid NFC',
-}
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="ja" translate="no">
-      <body>{children}</body>
-    </html>
-  )
+export default function Page() {
+  return <div>disabled</div>
 }
