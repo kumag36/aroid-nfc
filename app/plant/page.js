678bec40ca52873ce136c1b5927ede5dbbf0f42e
kumag36 <kumajuko@gmail.com>
2026-03-31 13:08:24 +0900

    Update page.js

--- app/plant/page.js
+++ app/plant/page.js
@@ -1,1 +1,16 @@
-page.js
+export default function Page() {
+  return (
+    <div style={{
+      background: '#020617',
+      minHeight: '100vh',
+      color: '#fff',
+      padding: '20px'
+    }}>
+      <h1>図鑑一覧</h1>
+
+      <a href="/plant/test">
+        テストページ
+      </a>
+    </div>
+  )
+}
