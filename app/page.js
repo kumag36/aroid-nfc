eb843d8868f196c937f8a8829a6a3b09d85cb93f
kumag36 <kumajuko@gmail.com>
2026-03-31 09:26:30 +0900

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,7 @@
-﻿import Link from 'next/link'
-
-export default function Home() {
+export default function Page() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>🌱 アロイド図鑑</h1>
-
-      <Link href="/plant">
-        👉 図鑑を見る
-      </Link>
+    <div>
+      OK
     </div>
   )
 }
