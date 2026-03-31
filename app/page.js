e5e619053d2f757b6a78856d866a2d1a3bce00a3
kumag36 <kumajuko@gmail.com>
2026-03-31 12:59:33 +0900

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
 export const dynamic = 'force-dynamic'
 
 export default function Page() {
-  return <div>Top</div>
+  return (
+    <div>
+      <h1>ざまくりプランツ図鑑</h1>
+      <a href="/plant">図鑑一覧へ</a>
+    </div>
+  )
 }
