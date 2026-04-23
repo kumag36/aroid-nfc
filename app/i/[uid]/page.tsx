b78bb0f5a3638842581698385be2229733d7870c
kumag36 <>
2026-04-24 01:07:31 +0900

    fix i route

--- app/i/[uid]/page.tsx
+++ app/i/[uid]/page.tsx
@@ -1,25 +1,3 @@
-import { redirect } from 'next/navigation'
-
-export default async function Page(props: any) {
-  const params = await props.params
-  const uid = params.uid
-
-  const res = await fetch(
-    `https://wasawygnvsvolkdrfjgs.supabase.co/rest/v1/items?select=slug&id=eq.${uid}`,
-    {
-      headers: {
-        apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`
-      },
-      cache: 'no-store'
-    }
-  )
-
-  const data = await res.json()
-
-  if (!data.length) {
-    return <div>UID not found</div>
-  }
-
-  redirect(`/plant/${data[0].slug}`)
+export default function Page() {
+  return <div>HELLO_I_ROUTE</div>
 }
