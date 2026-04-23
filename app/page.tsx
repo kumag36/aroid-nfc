4f730fd275b9c7ce32bc72da81a77b007ff9a543
kumag36 <>
2026-04-24 01:01:26 +0900

    add i route

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,15 @@
 ﻿import Link from 'next/link'
 
-export default async function Home() {
-  const res = await fetch("https://wasawygnvsvolkdrfjgs.supabase.co/rest/v1/plants?select=*", {
-    headers: {
-      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
-      Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
-    },
-    cache: "no-store"
-  })
-
-  const plants = await res.json()
-
+export default function Home() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>図鑑一覧</h1>
+    <main style={{ padding: 20 }}>
+      <h1>ざまくり図鑑</h1>
 
-      {plants.map((plant) => (
-        <Link key={plant.slug} href={/plant/}>
-          <div style={{
-            padding: 16,
-            marginBottom: 10,
-            background: '#0f172a',
-            borderRadius: 10,
-            cursor: 'pointer'
-          }}>
-            🌿 {plant.name}
-          </div>
-        </Link>
-      ))}
-    </div>
+      <p>テストリンク</p>
+
+      <Link href="/i/ZMK-000001">
+        ZMK-000001
+      </Link>
+    </main>
   )
 }
--- app/plant/[slug]/page.js
+++
@@ -1,9 +0,0 @@
-export const dynamic = 'force-dynamic'
-
-export default function Page({ params }) {
-  return (
-    <div style={{ color: '#fff', padding: '20px' }}>
-      slug: {params.slug}
-    </div>
-  )
-}
